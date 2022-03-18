ba3367cdd46744b0d4cd00a742c76e6f61a36f34
Madachii <roluxion@gmail.com>
2022-03-18 15:18:13 -0600

    Adding Player and AI

--- OOP-Connect4/Program.cs
+++ OOP-Connect4/Program.cs
@@ -8,6 +8,29 @@ namespace OOP_Connect4
 {
     internal class Program
     {
+        class Player
+        {
+            public string Name { get; set; }
+            public char Tile { get; set; }
+            public int Score { get; set; }
+            public bool Win { get; set; }
+
+            public virtual int chooseColumn()
+            {
+                Console.Write($"\n{Name}, where would you like to place your tile? ");
+                int value = int.Parse(Console.ReadLine());
+                return value;
+            }
+        }
+        class AI : Player
+        {
+            public override int chooseColumn()
+            {
+                Random number = new Random();
+                int tileAI = number.Next() % 7 + 1;
+                return tileAI;
+            }
+        }
         static void Main(string[] args)
         {
         }
