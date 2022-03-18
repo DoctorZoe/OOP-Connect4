528b46d55fae41bbcece4d408d9bf9d8f7e84716
Pedro Ferreira <yami_los@hotmail.com>
2022-03-18 16:05:46 -0600

    Fixing placement of classes and adding comments to code.

--- OOP-Connect4/Program.cs
+++ OOP-Connect4/Program.cs
@@ -6,101 +6,101 @@ using System.Threading.Tasks;
 
 namespace OOP_Connect4
 {
+    class Player
+    {
+        public string Name { get; set; } //Current player name
+        public char Tile { get; set; } //The current tile of the player
+        public int Score { get; set; } //How many wins the player has obtained in the session
+        public bool Win { get; set; } //If the player has won
+
+        public virtual int chooseColumn() //The player enters a value of a column to place their tile and that value gets returned
+        {
+            Console.Write($"\n{Name}, where would you like to place your tile? ");
+            int value = int.Parse(Console.ReadLine());
+            return value;
+        }
+    }
+    class AI : Player
+    {
+        public override int chooseColumn() //AI will pick a value at random between 1-7 to place their tile
+        {
+            Random number = new Random();
+            int tileAI = number.Next() % 7 + 1;
+            return tileAI;
+        }
+    }
+    static class Board
+    {
+        static public int turnCounter = 1; //counter for number of turns, starts at one for the first turn.
+        static public char[,] cleanBoard = { { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
+                                                 { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
+                                                 { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
+                                                 { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
+                                                 { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
+                                                 { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
+                                                 { ' ', '1', '2', '3', '4', '5', '6', '7', ' ' } }; //basic, clean board.
+        static public char[,] board; //actual board being used.
+        static public void NewBoard() //function for clearing the board.
+        {
+            board = cleanBoard;
+        }
+        static public void Display() //function for displaying the board.
+        {
+            Console.WriteLine();
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Initializer
+    {
+        public int NumOfUsers { get; set; } //Current number of players
+        public string Player1Name { get; set; } //Name of player 1
+        public string Player2Name { get; set; } //Name of player 2, "AI" if only one player is chosen
+
+        public void NewGame() //To be called in order to start a new game
+        {
+            Board.NewBoard();
+            Console.WriteLine("Welcome user! How many people will be playing?");
+            while (NumOfUsers != 1 && NumOfUsers != 2) //Wait for correct number of players
+            {
+                string input = Console.ReadLine();
+                int value = 0;
+                if (int.TryParse(input, out value)) //Ensure text will not crash the program
+                {
+                    if (value != 1 && value != 2)
+                    {
+                        Console.WriteLine("\nSorry! 1 or 2 players only!"); //Tell how many players are allowed
+                    }
+                }
+                else Console.WriteLine("\nPlease enter a number!"); //Tell only numbers
+                NumOfUsers = value; //Assign value to number of users for the class
+            }
+            switch (NumOfUsers) //Based on number of users get names for either 1 or 2 players and set them accordingly
+            {
+                case 1:
+                    Console.WriteLine("\n1 player, great!");
+                    Console.Write("Name: ");
+                    Player1Name = Console.ReadLine();
+                    Player2Name = "AI"; //With only 1 player, player 2 is AI
+                    break;
+                default:
+                    Console.WriteLine("\n2 players, good luck!");
+                    Console.Write("Player 1 Name: ");
+                    Player1Name = Console.ReadLine();
+                    Console.Write("Player 2 Name: ");
+                    Player2Name = Console.ReadLine();
+                    break;
+            }
+        }
+    }
     internal class Program
     {
-        class Player
-        {
-            public string Name { get; set; } //Current player name
-            public char Tile { get; set; } //The current tile of the player
-            public int Score { get; set; } //How many wins the player has obtained in the session
-            public bool Win { get; set; } //If the player has won
-
-            public virtual int chooseColumn() //The player enters a value of a column to place their tile and that value gets returned
-            {
-                Console.Write($"\n{Name}, where would you like to place your tile? ");
-                int value = int.Parse(Console.ReadLine());
-                return value;
-            }
-        }
-        class AI : Player
-        {
-            public override int chooseColumn() //AI will pick a value at random between 1-7 to place their tile
-            {
-                Random number = new Random();
-                int tileAI = number.Next() % 7 + 1;
-                return tileAI;
-            }
-        }
-        static class Board
-        {
-            static public int turnCounter = 1;
-            static public char[,] cleanBoard = { { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
-                                                 { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
-                                                 { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
-                                                 { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
-                                                 { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
-                                                 { '|', '#', '#', '#', '#', '#', '#', '#', '|' },
-                                                 { ' ', '1', '2', '3', '4', '5', '6', '7', ' ' } };
-            static public char[,] board;
-            static public void NewBoard()
-            {
-                board = cleanBoard;    
-            }
-            static public void Display()
-            {
-                Console.WriteLine();
-                for (int i = 0; i < board.GetLength(0); i++)
-                {
-                    for (int j = 0; j < board.GetLength(1); j++)
-                    {
-                        Console.Write(board[i, j] + " ");
-                    }
-                    Console.WriteLine();
-                }
-            }
-        }
-        class Initializer
-        {
-            public int NumOfUsers { get; set; } //Current number of players
-            public string Player1Name { get; set; } //Name of player 1
-            public string Player2Name { get; set; } //Name of player 2, "AI" if only one player is chosen
-
-            public void NewGame() //To be called in order to start a new game
-            {
-                Board.NewBoard();
-                Console.WriteLine("Welcome user! How many people will be playing?");
-                while (NumOfUsers != 1 && NumOfUsers != 2) //Wait for correct number of players
-                {
-                    string input = Console.ReadLine();
-                    int value = 0;
-                    if (int.TryParse(input, out value)) //Ensure text will not crash the program
-                    {
-                        if (value != 1 && value != 2)
-                        {
-                            Console.WriteLine("\nSorry! 1 or 2 players only!"); //Tell how many players are allowed
-                        }
-                    }
-                    else Console.WriteLine("\nPlease enter a number!"); //Tell only numbers
-                    NumOfUsers = value; //Assign value to number of users for the class
-                }
-                switch (NumOfUsers) //Based on number of users get names for either 1 or 2 players and set them accordingly
-                {
-                    case 1:
-                        Console.WriteLine("\n1 player, great!");
-                        Console.Write("Name: ");
-                        Player1Name = Console.ReadLine();
-                        Player2Name = "AI"; //With only 1 player, player 2 is AI
-                        break;
-                    default:
-                        Console.WriteLine("\n2 players, good luck!");
-                        Console.Write("Player 1 Name: ");
-                        Player1Name = Console.ReadLine();
-                        Console.Write("Player 2 Name: ");
-                        Player2Name = Console.ReadLine();
-                        break;
-                }
-            }
-        }
         static void Main(string[] args)
         {
             Initializer game = new Initializer(); //Create instance of game
